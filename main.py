3cb01eb6b916c90731f8a1e1919b5b31a7ec8241
tijoy <tijoy.work@gmail.com>
2024-12-02 15:51:51 +0300

    up

--- main.py
+++ main.py
@@ -21,34 +21,23 @@ lines = 2
 columns = 4
 start_survival_points = 15
 selected_items = {
-    "1": (3, 25),  # rifle ip:1
-    "2": (3, 20),  # axe ip:2
-    "3": (2, 15),  # pistol ip:3
+    "3": (2, 15),  # pistol ip:3 key = p
+    "4": (2, 15),  # ammo ip:4 key = a
+    "2": (3, 20),  # axe ip:2 key = x
 }
 selected_items_without_parametrs = []
 unselected_items = {
-    "4": (2, 15),  # ammo ip:4
-    "5": (2, 20),  # medkit ip:5
-    "6": (2, 20),  # supplies ip:6
-    "7": (2, 20),  # crossbow ip:7
-    "8": (1, 5),  # inhaler ip:8
-    "9": (1, 15),  # knife ip:9
-    "10": (1, 25),  # talisman ip:10
-    "11": (1, 15),  # flask ip:11
-    "12": (1, 10),  # antidot ip:12
+    "1": (3, 25),  # rifle ip:1 key = r
+    "8": (1, 5),  # inhaler ip:8 key = i
+    "9": (1, 15),  # knife ip:9 key = k
+    "5": (2, 20),  # medkit ip:5 key = m
+    "6": (2, 20),  # supplies ip:6 key = s
+    "7": (2, 20),  # crossbow ip:7 key = c
+    "10": (1, 25),  # talisman ip:10 key = t
+    "11": (1, 15),  # flask ip:11 key = f
+    "12": (1, 10),  # antidot ip:12 key = d
 }
-flag_rifle = 0
-flag_axe = 0
 inventory = [["" for _ in range(columns)] for _ in range(lines)]
-
-
-# показать инвентарь
-def show_inventoru():
-    for _ in range(lines):
-        print(inventory[_])
-
-
-# доделать
 
 
 def point_calculator(selected_items, unselected_items):
@@ -61,47 +50,206 @@ def point_calculator(selected_items, unselected_items):
 
 
 def appened_inventori(selected_items):
+    flag_rifle = 0
+    flag_axe = 0
     if sum(items[item][0] for item in selected_items) <= (lines * columns):
         selected_items_without_parametrs = sorted(list(map(int, selected_items.keys())))
-        if "r" in selected_items_without_parametrs:
+        if 1 in selected_items_without_parametrs:
             flag_rifle = 1
+        if 2 in selected_items_without_parametrs:
+            flag_axe = 1
+        # начало с  axe and rifle
+        if flag_rifle + flag_axe == 2 and (
+            3 in selected_items_without_parametrs
+            or 4 in selected_items_without_parametrs
+            or 5 in selected_items_without_parametrs
+            or 6 in selected_items_without_parametrs
+            or 7 in selected_items_without_parametrs
+        ):
             for _ in range(3):
                 inventory[0][_] = "r"
-        if "a" in selected_items_without_parametrs:
-            flag_axe = 1
             for _ in range(3):
-                inventory[1][_] = "a"
-        if flag_axe + flag_rifle == 2:
-            # and (
-            #     "3" in selected_items_without_parametrs
-            #     or "4" in selected_items_without_parametrs
-            #     or "5" in selected_items_without_parametrs
-            #     or "6" in selected_items_without_parametrs
-            #     or "7" in selected_items_without_parametrs
-            # ):
-
-            if "3" in selected_items_without_parametrs:
+                inventory[1][_] = "x"
+            if 3 in selected_items_without_parametrs:
                 key = "p"
-                for _ in range(2):
-                    inventory[_][2] = key
-            if "4" in selected_items_without_parametrs:
+            elif 4 in selected_items_without_parametrs:
                 key = "a"
-                for _ in range(2):
-                    inventory[_][2] = key
-            if "5" in selected_items_without_parametrs:
+            elif 5 in selected_items_without_parametrs:
                 key = "m"
-                for _ in range(2):
-                    inventory[_][2] = key
-            if "6" in selected_items_without_parametrs:
+            elif 6 in selected_items_without_parametrs:
                 key = "s"
-                for _ in range(2):
-                    inventory[_][2] = key
-            if "7" in selected_items_without_parametrs:
+            elif 7 in selected_items_without_parametrs:
                 key = "c"
-                for _ in range(2):
-                    inventory[_][2] = key
+            for _ in range(2):
+                inventory[_][3] = key
+        elif flag_rifle + flag_axe == 2 and (
+            8 in selected_items_without_parametrs
+            or 9 in selected_items_without_parametrs
+            or 10 in selected_items_without_parametrs
+            or 11 in selected_items_without_parametrs
+            or 12 in selected_items_without_parametrs
+        ):
+            flag_posishon = 0
+            if 8 in selected_items_without_parametrs:
+                key = "i"
+                inventory[flag_posishon][3] = key
+                flag_posishon = 1
 
-    return inventory
+            if 9 in selected_items_without_parametrs:
+                key = "k"
+                inventory[flag_posishon][3] = key
+                flag_posishon = 1
+
+            if 10 in selected_items_without_parametrs:
+                key = "t"
+                inventory[flag_posishon][3] = key
+                flag_posishon = 1
+
+            if 11 in selected_items_without_parametrs:
+                key = "f"
+                inventory[flag_posishon][3] = key
+                flag_posishon = 1
+
+            if 12 in selected_items_without_parametrs:
+                key = "d"
+                inventory[flag_posishon][3] = key
+                flag_posishon = 1
+        # конец с  axe and rifle
+        # начало с  axe or rifle
+        flag_medium_size_item = 0
+        if flag_rifle + flag_axe == 1 and (
+            3 in selected_items_without_parametrs
+            or 4 in selected_items_without_parametrs
+            or 5 in selected_items_without_parametrs
+            or 6 in selected_items_without_parametrs
+            or 7 in selected_items_without_parametrs
+            or 8 in selected_items_without_parametrs
+            or 9 in selected_items_without_parametrs
+            or 10 in selected_items_without_parametrs
+            or 11 in selected_items_without_parametrs
+            or 12 in selected_items_without_parametrs
+        ):
+            flag_posishon = 0
+            if 1 in selected_items_without_parametrs:
+                flag_rifle = 1
+                for _ in range(3):
+                    inventory[0][_] = "r"
+
+            if 2 in selected_items_without_parametrs:
+                flag_axe = 1
+                for _ in range(3):
+                    inventory[0][_] = "x"
+
+            if 3 in selected_items_without_parametrs:
+                flag_medium_size_item = 1
+                key = "p"
+                if flag_posishon == 0:
+                    flag_posishon = 1
+                    for _ in range(2):
+                        inventory[_][3] = key
+                elif flag_posishon == 1:
+                    for _ in range(2):
+                        inventory[1][_] = key
+
+            if 4 in selected_items_without_parametrs:
+                flag_medium_size_item = 1
+                key = "a"
+                if flag_posishon == 0:
+                    flag_posishon = 1
+                    for _ in range(2):
+                        inventory[_][3] = key
+                elif flag_posishon == 1:
+                    for _ in range(2):
+                        inventory[1][_] = key
+
+            if 5 in selected_items_without_parametrs:
+                flag_medium_size_item = 1
+                key = "m"
+                if flag_posishon == 0:
+                    flag_posishon = 1
+                    for _ in range(2):
+                        inventory[_][3] = key
+                elif flag_posishon == 1:
+                    for _ in range(2):
+                        inventory[1][_] = key
+
+            if 6 in selected_items_without_parametrs:
+                flag_medium_size_item = 1
+                key = "s"
+                if flag_posishon == 0:
+                    flag_posishon = 1
+                    for _ in range(2):
+                        inventory[_][3] = key
+                elif flag_posishon == 1:
+                    for _ in range(2):
+                        inventory[1][_] = key
+
+            if 7 in selected_items_without_parametrs:
+                flag_medium_size_item = 1
+                key = "c"
+                if flag_posishon == 0:
+                    flag_posishon = 1
+                    for _ in range(2):
+                        inventory[_][3] = key
+                elif flag_posishon == 1:
+                    for _ in range(2):
+                        inventory[1][_] = key
+
+            if 8 in selected_items_without_parametrs:
+                key = "i"
+                if flag_posishon == 0:
+                    flag_posishon = 1
+                    inventory[0][3] = key
+                elif flag_posishon == 1:
+                    flag_posishon = 2
+                    inventory[1][0] = key
+                elif flag_posishon == 2:
+                    flag_posishon = 3
+                    inventory[1][1] = key
+                elif flag_posishon == 3:
+                    flag_posishon = 4
+                    inventory[1][2] = key
+                elif flag_posishon == 4:
+                    if flag_medium_size_item == 0:
+                        flag_posishon = 5
+                        inventory[1][3] = key
+
+                inventory[flag_posishon][3] = key
+                flag_posishon = 1
+            if 9 in selected_items_without_parametrs:
+                key = "k"
+                inventory[flag_posishon][3] = key
+                flag_posishon = 1
+            if 10 in selected_items_without_parametrs:
+                key = "t"
+                inventory[flag_posishon][3] = key
+                flag_posishon = 1
+            if 11 in selected_items_without_parametrs:
+                key = "f"
+                inventory[flag_posishon][3] = key
+                flag_posishon = 1
+            if 12 in selected_items_without_parametrs:
+                key = "d"
+                inventory[flag_posishon][3] = key
+                flag_posishon = 1
+
+    # "1": (3, 25),  # rifle ip:1 key = r
+    # "2": (3, 20),  # axe ip:2 key = x
+
+    # "3": (2, 15),  # pistol ip:3 key = p
+    # "4": (2, 15),  # ammo ip:4 key = a
+    # "5": (2, 20),  # medkit ip:5 key = m
+    # "6": (2, 20),  # supplies ip:6 key = s
+    # "7": (2, 20),  # crossbow ip:7 key = c
+
+    # "8": (1, 5),  # inhaler ip:8 key = i
+    # "9": (1, 15),  # knife ip:9 key = k
+    # "10": (1, 25),  # talisman ip:10 key = t
+    # "11": (1, 15),  # flask ip:11 key = f
+    # "12": (1, 10),  # antidot ip:12 key = d
+
+    # конец с  axe or rifle
+    return f"{inventory[0]}\n{inventory[1]}"
 
 
 print(appened_inventori(selected_items))
